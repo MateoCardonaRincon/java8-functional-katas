02b2402ed6a7966949639460e9880cafff7bcc90
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-05-25 17:02:14 -0500

    Implementation of kata3 and kata3

--- src/test/java/katas/Kata3Test.java
+++ src/test/java/katas/Kata3Test.java
@@ -3,6 +3,8 @@ package katas;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 
 
@@ -10,6 +12,10 @@ public class Kata3Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata3.execute().size(), equalTo(4));
+        List<Integer> result= Kata3.execute();
+        String typeName = result.get(0).getClass().getTypeName();
+
+        Assert.assertThat(result.size(), equalTo(4));
+        Assert.assertThat(typeName, equalTo("java.lang.Integer"));
     }
 }
