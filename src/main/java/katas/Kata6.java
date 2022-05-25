9a3b5f44e463ee12bd8855428c6469c8172d12bf
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-05-25 18:26:56 -0500

    Implementation of Kata6

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,9 +1,15 @@
 package katas;
 
+import model.BoxArt;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -14,6 +20,16 @@ public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        Supplier<NullPointerException> noBoxArtFound = () -> {
+            throw new NullPointerException("No box-art found");
+        };
+
+        Stream<BoxArt> boxArtStream = movies.stream().flatMap(movie -> movie.getBoxarts().stream());
+
+        return boxArtStream.reduce((priorBoxart, currentBoxArt) ->
+                        priorBoxart.getWidth() * priorBoxart.getHeight() > currentBoxArt.getWidth() * currentBoxArt.getHeight() ?
+                                priorBoxart : currentBoxArt)
+                .orElseThrow(noBoxArtFound)
+                .getUrl();
     }
 }
